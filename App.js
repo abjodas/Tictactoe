008aab0d035bffd1085ad8afa2b2ee86ffac3a75
Abhishek <abjodas@gmail.com>
2022-04-05 00:09:19 +0530

    checking row and column

--- App.js
+++ App.js
@@ -6,25 +6,101 @@ import {
   View,
   ImageBackground,
   Pressable,
+  Alert,
 } from "react-native";
 import bg from "./assets/bg.jpeg";
 export default function App() {
   const [map, setMap] = useState([
-    ["x", "x", "O"], //1st row
-    ["O", "x", "x"], //2nd row
-    ["O", "O", "x"], //3rd row
+    ["", "", ""], //1st row
+    ["", "", ""], //2nd row
+    ["", "", ""], //3rd row
   ]);
+
+  const [currentTurn, setCurrentTurn] = useState("x");
+
   const onPress = (rowIndex, cellIndex) => {
     console.warn("hello", rowIndex, cellIndex);
+    if (map[rowIndex][cellIndex] != "") {
+      Alert.alert("Position already occupied");
+      return;
+    }
+
+    setMap((existingMap) => {
+      const updatedMap = [...existingMap];
+      updatedMap[rowIndex][cellIndex] = currentTurn;
+      return updatedMap;
+    });
+
+    setCurrentTurn(currentTurn == "x" ? "O" : "x");
+
+    checkWinningState();
+  };
+
+  const checkWinningState = () => {
+    //Check rows
+    for (let i = 0; i < 3; i++) {
+      const isRowXWinning = map[i].every((cell) => cell == "x");
+      const isRowOWinning = map[i].every((cell) => cell == "O");
+      if (isRowXWinning) {
+        Alert.alert("X won. Row");
+      }
+      if (isRowOWinning) {
+        Alert.alert("O won. Row");
+      }
+    }
+    //Check coumns
+    for (let col = 0; col < 3; col++) {
+      let isColumnXWinner = true;
+      let isColumnOWinner = true;
+
+      for (let row = 0; row < 3; row++) {
+        if (map[row][col] != "x") {
+          isColumnXWinner = false;
+        }
+        if (map[row][col] != "O") {
+          isColumnOWinner = false;
+        }
+      }
+      if (isColumnXWinner) {
+        Alert.alert("X won. Column");
+      }
+      if (isColumnOWinner) {
+        Alert.alert("O won. Column");
+      }
+    }
+    //Check diagonals
+    // for (let row = 0; row < 3; row++) {
+    //   let isDiagonalXWinner = true;
+    //   let isDiagonalOWinner = true;
+    //   for (let col = 0; col < 3; col++) {
+    //     if (map[row][col] != "x") {
+    //       if (row == col) {
+    //         isDiagonalXWinner = false;
+    //       }
+    //     }
+    //     if (map[row][col] != "O") {
+    //       if (row == col) {
+    //         isDiagonalOWinner = false;
+    //       }
+    //     }
+    //   }
+    //   if (isDiagonalXWinner) {
+    //     Alert.alert("X won. Diagonal");
+    //   }
+    //   if (isDiagonalOWinner) {
+    //     Alert.alert("O won. Diagonal");
+    //   }
+    // }
   };
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} style={styles.bg}>
         <View style={styles.map}>
           {map.map((row, rowIndex) => (
-            <View style={styles.row}>
+            <View key={`row-${rowIndex}`} style={styles.row}>
               {row.map((cell, cellIndex) => (
                 <Pressable
+                  key={`row-${rowIndex}-col-${cellIndex}`}
                   onPress={() => onPress(rowIndex, cellIndex)}
                   style={styles.cell}
                 >
@@ -101,8 +177,8 @@ const styles = StyleSheet.create({
     transform: [{ rotate: "-45deg" }],
   },
   map: {
-    borderWidth: 1,
-    borderColor: "white",
+    // borderWidth: 1,
+    // borderColor: "white",
     width: "80%",
     aspectRatio: 1,
   },
@@ -110,8 +186,9 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     flex: 1,
-    borderWidth: 1,
-    borderColor: "white",
+
+    // borderWidth: 1,
+    // borderColor: "white",
   },
   cross: {
     flex: 1,
